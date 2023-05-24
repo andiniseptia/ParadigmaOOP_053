29acee65700652e73da77cb045689ca12f42260c
andiniseptia <andiniseptiandri124@gmail.com>
2023-05-24 11:10:10 +0700

    Mendeklarasikan member class orang

--- paradigmaoop/paradigmaoop.cpp
+++ paradigmaoop/paradigmaoop.cpp
@@ -1,2 +1,21 @@
-// paradigmaoop.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <iostream>
+#include <string>
+using namespace std;
+
+class orang {
+public:
+	string nama;
+
+	orang(string pNama) :
+		nama(pNama) {
+		cout << "orang dibuat\n" << endl;
+	}
+	~orang() {
+		cout << "orang dihapus\n" << endl;
+	}
+
+	int jumlah(int a, int b) {
+		return a + b;
+	}
+
+};
