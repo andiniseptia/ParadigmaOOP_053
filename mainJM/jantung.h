d88c54cb31e5f3d6012b2a8090125e679af30121
andiniseptia <andiniseptiandri124@gmail.com>
2023-05-24 17:40:52 +0700

    Mendeskripsikan member pada class jantung

--- mainJM/jantung.h
+++ mainJM/jantung.h
@@ -1,1 +1,15 @@
-#pragma once
+#ifndef JANTUNG-H
+#define JANTUNG_H
+
+using namespace std;
+
+class jantung {
+public:
+	jantung() {
+		cout << "jantung dihidupkan\n";
+	}
+	~jantung() {
+		cout << "jantung dimatikan\n";
+	}
+};
+#endif
